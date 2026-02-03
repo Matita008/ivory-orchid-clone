8ecef8f35c50e83968eb6bddc1fe052275a7e94b
Matita008 <155455608+Matita008@users.noreply.github.com>
2026-02-03 19:45:57 +0100

    Fixed templates added a basse template the 404 page and index now uses the base.html template

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../frontend/templates")
 
 from backend import static, test
--- backend/static.py
+++ backend/static.py
@@ -10,6 +10,7 @@ from . import app
 FRONTEND = os.path.join(os.path.dirname(__file__), "..")
 FRONTEND_SPECIFIC = {"js": "js", "css": "css", "html": "static"}
 
+TEMPLATE_404 = "404.html"
 PAGE_404 = os.path.join(FRONTEND, "404.html")
 
 @app.errorhandler(404)
@@ -17,22 +18,21 @@ def load_static(e):
 	paths = set()
 	paths.add(FRONTEND)
 
+	try:
+		return render_template(request.path.split("/")[-1])
+	except:
+		print("Template " + request.path.split("/")[-1] + " not found")
+
 	filetype = request.path.split(".")[-1]
 	if(filetype in FRONTEND_SPECIFIC):
 		paths.add(os.path.join(FRONTEND, FRONTEND_SPECIFIC[filetype]))
 
 	for path in paths:
-		print("\nDebug: ")
 		path = safe_join(path, request.path.lstrip("/"))
-		print(path)
-		print("\n")
 		if(os.path.exists(path)):
-			try:
-				return render_template(path)
-			except:
-				return send_file(path)
+			return send_file(path)
 
 	try:
-		return render_template(PAGE_404), 404
+		return render_template(TEMPLATE_404), 404
 	except:
 		return send_file(PAGE_404), 404
